a11435b365163cde7c352ed55f71ba5ba5f2cc9d
abscosmos <joshua@dinoslice.com>
2024-05-12 00:18:19 -0500

    Simple runner.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
+use std::io;
+use std::io::Write;
 use itertools::Itertools;
-use crate::game_board::{FoundWord, GameBoard};
+use crate::game_board::GameBoard;
 use crate::word_list::WordList;
 
 mod game_board;
@@ -7,21 +9,21 @@ mod word_list;
 mod found_word;
 
 fn main() {
-    let board = "oatrihpshtnrenei";
+    let mut board= String::new();
 
-    let board = GameBoard::from_string(board).unwrap();
+    println!("Enter board as 16 continuous letters: ");
+    io::stdin().read_line(&mut board).expect("Read input from stdin");
 
-    println!("{board}");
+    let board = GameBoard::from_string(&board.trim()).expect("Invalid board input");
 
-    let mut list = WordList::load_from_file("dictionary/compiled_words.txt".as_ref()).unwrap();
+    println!("{board}\n");
+
+    let mut list = WordList::load_from_file("dictionary/CollinsScrabbleWords.txt".as_ref()).unwrap();
     list.retain_only_buildable(&board.letters());
     let words_trie = board.find_possible_sequences(&list.into_trie());
 
-    words_trie.iter()
-        .sorted()
-        .rev()
-        .take(5)
-        .for_each(|w| println!("{w:?}"));
-
-    println!("{}", words_trie.len());
+    for w in words_trie.iter().sorted().rev() {
+        println!("{w}\n");
+        io::stdin().read_line(&mut String::new()).unwrap();
+    }
 }
